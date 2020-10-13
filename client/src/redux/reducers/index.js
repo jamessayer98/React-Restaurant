8243183b8659b52b2bc2ecec78c014fa5900050d
James Sayer <james.sayer0908@gmail.com>
2020-10-13 16:47:32 +0100

    Add restaurant redux-saga

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,19 +1,23 @@
 import { combineReducers } from "redux";
 
 import auth from "./auth";
-import user from "./user";
 import toast from "./toast";
 import progress from "./progress";
+import restaurant from "./restaurant";
+import review from "./review";
+import user from "./user";
 
 const appReducer = combineReducers({
   auth,
   toast,
   progress,
-  user,
+  restaurant,
+  review,
+  user
 });
 
 const rootReducer = (state, action) => {
-  if (action.type === 'LOG_OUT' || action.type === 'REMOVE_PROFILE') {
+  if (action.type === "LOG_OUT" || action.type === "REMOVE_PROFILE") {
     return appReducer(undefined, action);
   }
 
---
+++ client/src/redux/reducers/restaurant.js
@@ -0,0 +1,87 @@
+import { handleActions } from "redux-actions";
+import { Success, Fail } from "../../utils/status";
+import {
+  GET_RESTAURANTS,
+  CREATE_RESTAURANT,
+  UPDATE_RESTAURANT,
+  DELETE_RESTAURANT,
+  SET_RESTAURANT_PARAMS
+} from "../constants";
+
+const initialState = {
+  restaurants: [],
+  currentRestaurant: null,
+  filteredResults: [],
+  count: -1,
+  params: {
+    page: 1,
+    limit: 5,
+    min: null,
+    max: null,
+    user: []
+  },
+  error: ""
+};
+
+export default handleActions(
+  {
+    [SET_RESTAURANT_PARAMS]: (state, { payload }) => ({
+      ...state,
+      params: {
+        ...state.params,
+        ...payload
+      }
+    }),
+    [Success(GET_RESTAURANTS)]: (state, { payload }) => ({
+      ...state,
+      restaurants: payload.restaurants,
+      count: payload.count,
+      error: null
+    }),
+    [Fail(GET_RESTAURANTS)]: (state, { payload }) => ({
+      ...state,
+      error: payload.data
+    }),
+    [Success(CREATE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        currentRestaurant: payload,
+        count: state.count + 1,
+        error: null
+      };
+    },
+    [Fail(CREATE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        error: payload.data
+      };
+    },
+    [Success(UPDATE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        currentRestaurant: payload,
+        error: null
+      };
+    },
+    [Fail(UPDATE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        error: payload.data
+      };
+    },
+    [Success(DELETE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        count: state.count - 1,
+        error: null
+      };
+    },
+    [Fail(DELETE_RESTAURANT)]: (state, { payload }) => {
+      return {
+        ...state,
+        error: payload.data
+      };
+    }
+  },
+  initialState
+);
